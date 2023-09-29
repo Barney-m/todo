1091abbb23bd53e9c8232a3fd0beb157ba0eb77a
Barney-m <kenshencu99@gmail.com>
2023-09-29 10:38:23 +0800

    TODO-11: [Documentation] README.md documentation

--- src/test/java/com/ks/todo/TodoApplicationTests.java
+++ src/test/java/com/ks/todo/TodoApplicationTests.java
@@ -1,15 +1,10 @@
 package com.ks.todo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @AutoConfigureTestDatabase
 @SpringBootTest
 class TodoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
